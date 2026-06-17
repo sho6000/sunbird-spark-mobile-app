0cac04f64b084c995313f47360572130789720a7
sho6000 <shounsalaji@gmail.com>
2026-06-17 13:26:09 +0530

    updated the waitforTimeout value in wdio.conf.ts file to 30000 milliseconds from 10000 milliseconds to allow more time for the elements to be found during the test execution.

--- mobile-automation/specs/android/anonymous_user_consumption/suite0-dismiss-onboarding.e2e.ts
+++ mobile-automation/specs/android/anonymous_user_consumption/suite0-dismiss-onboarding.e2e.ts
@@ -5,14 +5,6 @@ describe('Suite 0: Dismiss Onboarding', () => {
     it('should dismiss onboarding if present', async () => {
         await dismissOnboarding(browser);
     });
-
-    after('Summary of discovered content', async () => {
-    
-
-        await browser.saveScreenshot('../reports/android/test-results/suite0-home-after.png');
-
-    
-    });
     
 
 });
